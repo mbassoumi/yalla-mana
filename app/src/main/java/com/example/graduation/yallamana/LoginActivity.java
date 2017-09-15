a2e0ca80a95482035d1f4af07dbcf83c6a6148a3
m_7el <maisshelou@gmail.vom>
2017-09-15 11:21:00 -0700

    hi

--- app/src/main/java/com/example/graduation/yallamana/LoginActivity.java
+++ app/src/main/java/com/example/graduation/yallamana/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.graduation.yallamana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,27 +17,36 @@ public class LoginActivity extends AppCompatActivity {
 
 
     @BindView(R.id.signupButton)
-    Button SignupButton;
+    Button signupButton;
     @BindView(R.id.user_phone)
-    EditText UserPhone;
+    EditText userPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ButterKnife.bind(this);
         setContentView(R.layout.activity_login);
+       signupButton = (Button) findViewById(R.id.signupButton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Toast.makeText(LoginActivity.this,"signup",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+        });
+
     }
 
-    @OnClick(R.id.yallaButton)
-    void yallaGo() {
 
-    }
 
-    @OnClick(R.id.signupButton)
-    void signupGo() {
-        Navigator.navigateTosignup(LoginActivity.this);
-        finish();
-    }
 
 
 }
