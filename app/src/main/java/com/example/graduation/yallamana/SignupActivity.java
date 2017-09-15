97309da623a05d8d76baac23befb6afa79a2363c
m_7el <maisshelou@gmail.vom>
2017-09-15 12:39:03 -0700

    edit signup

--- app/src/main/java/com/example/graduation/yallamana/SignupActivity.java
+++ app/src/main/java/com/example/graduation/yallamana/SignupActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -26,21 +27,16 @@ public class SignupActivity extends AppCompatActivity {
         mToolBar.setTitleTextColor(getResources().getColor(R.color.white));
         mToolBar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
         setSupportActionBar(mToolBar);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-       // case android.R.id.home:// back home
-        if (item .getItemId()== R.id.backButton) {
-            Toast.makeText(getApplicationContext(), "back", Toast.LENGTH_SHORT).show();
-            Intent my = new Intent(SignupActivity.this, LoginActivity.class);
-            startActivity(my);
-            return true;
+
+   mToolBar.setNavigationOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(SignupActivity.this, "Back clicked!",     Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
         }
+    });
 
-        return super.onOptionsItemSelected(item);
-    }
-
-
-}
+}}
 
