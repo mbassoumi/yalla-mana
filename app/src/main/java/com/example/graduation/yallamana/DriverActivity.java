668f1b3d8f21e085b1bf8603ce26c483e70e53ef
m_7el <maisshelou@gmail.vom>
2017-09-17 09:29:34 -0700

    edit signup

--- app/src/main/java/com/example/graduation/yallamana/DriverActivity.java
+++ app/src/main/java/com/example/graduation/yallamana/DriverActivity.java
@@ -1,14 +1,35 @@
 package com.example.graduation.yallamana;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class DriverActivity extends AppCompatActivity {
 
+    Toolbar  mToolBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
+        mToolBar = (Toolbar) findViewById(R.id.toolbarDriver);
+        mToolBar.setTitle(R.string.driver_toolbar);
+        mToolBar.setTitleTextColor(getResources().getColor(R.color.white));
+        mToolBar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        setSupportActionBar(mToolBar);
+
+        mToolBar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(DriverActivity.this, SplashActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/graduation/yallamana/SignupActivity.java
+++ app/src/main/java/com/example/graduation/yallamana/SignupActivity.java
@@ -1,6 +1,10 @@
 package com.example.graduation.yallamana;
 
+
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,6 +12,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -17,12 +22,15 @@ import butterknife.ButterKnife;
 public class SignupActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
-    Toolbar appToolbar;
+    Toolbar  mToolBar;
+    ImageButton driverButton;
+    ImageButton riderButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-      Toolbar  mToolBar = (Toolbar) findViewById(R.id.toolbar);
+       mToolBar = (Toolbar) findViewById(R.id.toolbar);
         mToolBar.setTitle(R.string.new_user);
         mToolBar.setTitleTextColor(getResources().getColor(R.color.white));
         mToolBar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
@@ -38,5 +46,33 @@ public class SignupActivity extends AppCompatActivity {
         }
     });
 
+
+   riderButton = (ImageButton) findViewById(R.id.rider);
+
+		riderButton.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+
+
+
+        }
+
+    });
+
+    driverButton = (ImageButton) findViewById(R.id.driver);
+
+		driverButton.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+            Intent intent = new Intent( SignupActivity.this,DriverActivity.class);
+            startActivity(intent);
+            finish();
+
+
+        }
+
+    });
 }}
 
