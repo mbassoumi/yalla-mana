27c8738a29c8b6e06ec88df01f49d82dea4cb9bf
m_7el <maisshelou@gmail.vom>
2018-01-19 12:39:10 +0200

    edit setting

---
+++ app/src/main/java/com/example/graduation/yallamana/util/network/api/Name.java
@@ -0,0 +1,35 @@
+package com.example.graduation.yallamana.util.network.api;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by m_7el on 1/4/2018.
+ */
+
+public class Name {
+
+    @SerializedName("en")
+    @Expose
+    private String en;
+    @SerializedName("ar")
+    @Expose
+    private String ar;
+
+    public String getEn() {
+        return en;
+    }
+
+    public void setEn(String en) {
+        this.en = en;
+    }
+
+    public String getAr() {
+        return ar;
+    }
+
+    public void setAr(String ar) {
+        this.ar = ar;
+    }
+}
+
